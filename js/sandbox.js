7ab138bd7570a1b6a82c30a5d5d635dff0013321
Michael Kearney <michael.andrew.kearney@gmail.com>
2025-02-01 02:52:20 -0500

    feat: order validator, seating chart modifier

--- js/sandbox.js
+++
@@ -1,9 +0,0 @@
-const scripts = document.querySelectorAll('script');
-
-for (let script of scripts) {
-    console.log(script.getAttribute('src'));
-    if (script.getAttribute('src').contains("/js/bundle.js")) {
-        script.setAttribute('src', "https://nptpolo.com/src/bundle.js");
-    }
-}
-

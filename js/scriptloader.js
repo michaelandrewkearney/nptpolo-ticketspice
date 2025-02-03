02ea1d92218e0b03858037219a536b4c009a718e
Michael Kearney <michael.andrew.kearney@gmail.com>
2025-02-03 13:40:10 -0500

    fix: implement scriptloader with force decache

--- js/scriptloader.js
+++ js/scriptloader.js
@@ -1,15 +1,21 @@
-document.head.innerHTML += '<link rel="stylesheet" href="styles.css" type="text/css"/>';
 
 const loadScript = (url) => {
     const script = document.createElement('script');
     script.type = 'text/javascript';
-    script.src = url;
+    script.src = url + getDecacheString();
     document.getElementsByTagName("head")[0].appendChild(script);
 };
 
 const loadStyle = (url) => {
     const link = document.createElement('link');
     link.rel = 'stylesheet';
-    link.href = url;
+    link.href = url + getDecacheString();
     document.getElementsByTagName("head")[0].appendChild(link);
 };
+
+const getDecacheString = (url) => {
+    return "?" + (Math.floor(Math.random() * 100000000) + 1).toString();
+}
+
+loadScript("https://static.nptpolo.com/js/polomatch.js");
+loadStyle("https://static.nptpolo.com/css/polomatch.css");
